db6f2813e20cc1fbb47921962fbe1aa0ca09029b
dixraelzin <89050796+dixraelzin@users.noreply.github.com>
2026-01-28 15:00:53 -0300

    Add functions to get and save history in localStorage

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,7 @@
+const KEY="hist";
+export const getHist=()=>JSON.parse(localStorage.getItem(KEY)||"[]");
+export const saveRec=r=>{
+  const h=getHist();h.unshift(r);
+  localStorage.setItem(KEY,JSON.stringify(h));
+};
 
